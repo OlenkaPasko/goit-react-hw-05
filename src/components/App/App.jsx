f9ba21d560356aa254d8369437d3037c296c54dd
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-14 14:59:04 +0300

    HomePage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+import HomePage from "../pages/HomePage";
 
 export default function App() {
-    return (
-      <div>
-        <h1>home work</h1>
-      </div>
-    );
-
-
+  return (
+    <div>
+      <h1>home work</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route />
+        <Route />
+        <Route />
+        <Route />
+      </Routes>
+    </div>
+  );
 }
