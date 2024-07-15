1bad8ac84f854ca07bb1ee5af28c97d9d10eb6e0
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-15 07:34:22 +0300

    getTrending

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,52 +1,29 @@
 import { useEffect, useState } from "react";
-//import { Link, useLocation } from "react-router-dom";
-import axios from "axios";
-import {getTrending} from "../../api"
+import { getTrending } from "../../api";
 
 export default function HomePage() {
-  //http запит
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  // HTTP запит
   useEffect(() => {
-    async function getTrendingMov() {
-      const response = await axios.get(getTrending);
-      console.log(response);
+    async function fetchTrendingMovies() {
+      const response = await getTrending();
+      setTrendingMovies(response.results);
+      console.log(response.results);
     }
-    getTrendingMov();
+    fetchTrendingMovies();
   }, []);
+
   return (
     <div>
       <h1>Запит</h1>
+      <ul>
+        {trendingMovies.map((movie) => (
+          <li key={movie.id}>{movie.title || movie.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 
-
-//export default function HomePage() {
-  //const [trendMovies, setTrendMovies] = useState([]);
-   // const location = useLocation();
-   // const [isLoading, setIsLoading] = useState(false);
-  
-    //useEffect(() => {
-   // getTrending().then((data) => {
-      //  setTrendMovies(data.results);
-       //  setIsLoading(false);
-    //});
-  //}, []);
-
-  //return (
-   // <>
-     // <ul>
-     //   {trendMovies.map((item) => {
-       //   const { id, title, name } = item;
-       //   return (
-        //   <li key={id}>
-        //      <Link to={`/movies/${id}`} state={{ from: location }}>
-        //        {title || name}
-        //      </Link>
-         //   </li>
-        //  );
-       // })}
-     // </ul>
-   // </>
- // );
-//}
