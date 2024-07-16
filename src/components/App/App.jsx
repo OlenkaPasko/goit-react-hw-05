6a373693338ddc8aa88fae356b55b5936967ac67
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-16 15:21:08 +0300

    MovieCast

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage/HomePage";
 import MoviesPage from "../pages/MoviesPage";
 import Navigation from "../Navigation/NavLink";
+import MovieCast from "../MovieCast/MovieCast";
 
 import MovieDetailsPage from "../pages/MovieDetailsPage/MovieDetailsPage";
 
@@ -16,7 +17,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
         <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
-        <Route />
+        <Route path="/movies/:movieId/cast" element={<MovieCast/>} />
         <Route />
       </Routes>
     </div>
---
+++ src/components/MovieCast/MovieCast.jsx
@@ -0,0 +1,4 @@
+export default function MovieCast() {
+    return (<div>MovieCast</div>);
+
+}
