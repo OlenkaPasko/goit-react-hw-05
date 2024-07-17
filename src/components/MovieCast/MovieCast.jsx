8ada86e10ac82cca62212a64c9ef27e622dbbe25
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-18 01:30:55 +0300

    castLis

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,30 +1,64 @@
-
 import { useEffect, useState } from "react";
-import { getMovieCast } from "../../api"
+import { useParams } from "react-router-dom";
+import { getMovieCast } from "../../api";
 
 export default function MovieCast() {
-    const [castList, setCastList] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+const { movieId } = useParams();
+  const [castList, setCastList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    
+  //HTTp запит
 
-    //HTTp запит
+  useEffect(() => {
+    async function fetchMovieCast() {
+      try {
+        setLoading(true);
+        const response = await getMovieCast(movieId);
+        setCastList(response.cast);
+        console.log(response);
+      } catch (error) {
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchMovieCast();
+  }, [movieId]);
 
-    useEffect(() => {
-        async function fetchMovieCast() {
-            
-            try {
-                setLoading(true);
-                const response = await getMovieCast(movie_id);
-                
-            }
-            catch (error) {
-                
-            }
-        }
-    })
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-    return (<div>MovieCast</div>);
+  if (error) {
+    return <p>Error loading cast list.</p>;
+  }
 
+  if (!castList) {
+    return null;
+  }
+  const { character, name, profile_path, id } = castList;
+  return (
+    <div>
+      {castList.map((actor) => (
+        <div key={actor.id}>
+          {actor.profile_path ? (
+            <img
+              src={`https://image.tmdb.org/t/p/original${actor.profile_path}`}
+              alt={actor.name}
+              width="200"
+            />
+          ) : (
+            <img
+              src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg?w=2000"
+              alt="404"
+              width="120px"
+            />
+          )}
+          <p>{actor.name}</p>
+          <p>Character: {actor.character}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
