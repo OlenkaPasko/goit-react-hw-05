1b34b56263ce700f9662ab9ca89337745c6396ee
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-20 11:38:33 +0300

    MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,19 +1,23 @@
-import { NavLink } from "react-router-dom";
-import clsx from "clsx";
-import css from "./MovieList.module.css"
+import { Link, useLocation } from "react-router-dom";
+//import clsx from "clsx";
+import css from "./MovieList.module.css";
 
-const makeNavLinkClass = ({ isActive }) => {
-  return clsx(css.link, isActive && css.active);
-};
+///const makeNavLinkClass = ({ isActive }) => {
+  ///return clsx(css.link, isActive && css.active);
+///};
 export default function MovieList({ movies }) {
+  const location = useLocation();
   return (
     <div className={css.movieList}>
       <ul className={css.lists}>
         {movies.map((movie) => (
           <li className={css.list} key={movie.id}>
-            <NavLink to={`/movies/${movie.id}`} className={makeNavLinkClass}>
+            <Link
+              to={`/movies/${movie.id}`}
+              state={{ from: location }}
+            >
               {movie.title || movie.name}
-            </NavLink>
+            </Link>
           </li>
         ))}
       </ul>
